aa8bb0262e51eba8710df4c3faab2700f600afe2
rocktimsaikia <rocktimthedev@gmail.com>
2024-09-10 22:06:42 +0530

    Add remove_every_other function in C

---
+++ cs/c/remove-elements.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+void remove_every_other (size_t *length, int array[*length]){
+	// mutate the array in-place
+  size_t new_length = 0;
+  for (size_t i=0; i < *length; i+=2){
+      array[new_length] = array[i];
+      new_length++;
+  }
+  *length = new_length;
+}
